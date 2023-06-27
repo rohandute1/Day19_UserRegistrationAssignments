e32a83d911b9d4c6da73bef3742a92e29d999e8e
Rohan <rohandute1@gmail.com>
2023-06-27 23:38:42 +0530

    [Rohan] Successfully added regex for valid First name

--- Day19_UserRegistrationAssignments/Program.cs
+++ Day19_UserRegistrationAssignments/Program.cs
@@ -11,6 +11,17 @@ namespace Day19_UserRegistrationAssignments
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular expression problem");
+            Console.WriteLine(" Please select program from following");
+            Console.WriteLine(" 1.Valid First name\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ValidFirstName validator = new ValidFirstName();
+                    validator.ValidName();
+                    break;
+            }
+            
             Console.ReadLine(); 
         }
     }
---
+++ Day19_UserRegistrationAssignments/ValidFirstName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day19_UserRegistrationAssignments
+{
+    public class ValidFirstName
+    {
+        public void ValidName()
+        {
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine(); 
+
+            if (IsValidFirstName(firstName))
+            {
+                Console.WriteLine("Valid first name entered.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name entered.");
+            }
+        }
+
+        private bool IsValidFirstName(string firstName)
+        {
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            return Regex.IsMatch(firstName, pattern);
+        }
+    }
+}
